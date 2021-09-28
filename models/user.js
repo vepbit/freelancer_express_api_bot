76a69736197b7bb23e1bfba5080ccf7e94e13eb0
Andrey <dabog19@yahoo.com>
2021-09-28 23:59:58 +0300

    Changed default notification status

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const User = database_user.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
     chatId: {type: DataTypes.STRING, unique: true},
     tags: {type: DataTypes.STRING, defaultValue: ''},
-    autoupdate: {type: DataTypes.INTEGER, defaultValue: 'Off'},
+    autoupdate: {type: DataTypes.INTEGER, defaultValue: 'On'},
     project_time_ago:{type: DataTypes.STRING},
     project_type: {type: DataTypes.STRING, defaultValue: 'all'},
     user_data: {type: DataTypes.STRING},
